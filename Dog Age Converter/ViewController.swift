c1a7df19453c082a60e2d37d88f8b495d604958c
Clint Greive <clintgreive@Clints-MacBook-Air.local>
2014-10-08 18:43:45 +1000

    first commit

--- Dog Age Converter/ViewController.swift
+++ Dog Age Converter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsResultLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var humanYearsEnteredTextField: UITextField!
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertToDogYearsButton(sender: UIButton) {
+        
+        let optionalDoubleFromDogYearsResultLabel = Double((humanYearsEnteredTextField.text as NSString).doubleValue)
+        var dogYears:Double
+        
+        if optionalDoubleFromDogYearsResultLabel > 2 {
+            dogYears = (10.5 * 2) + (optionalDoubleFromDogYearsResultLabel - 2) * 4
+            commentsLabel.text = ""
+        } else {
+            dogYears = optionalDoubleFromDogYearsResultLabel * 10.5
+            commentsLabel.text = "It's only a puppy! 🐶"
+            commentsLabel.textColor = UIColor.yellowColor()
+        }
+        
+        dogYearsResultLabel.text = "\(dogYears)" + " years of age"
+        
+        humanYearsEnteredTextField.resignFirstResponder()
+        dogYearsResultLabel.hidden = false
+        humanYearsEnteredTextField.text = ""
+        convertButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        convertButton.setTitle("Converted", forState: UIControlState.Normal)
+    }
+    
 }
 
